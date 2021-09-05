8dd1a0dcc9cd96bc66cc9401644b40dc05cd404a
Pandam <victorvgv96@hotmail.es>
2021-09-05 09:49:28 +0200

    --ammend

---
+++ Assets/Scripts/Isaac/PlayerStats.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static PlayerStats instance;
+
+    private int health = 10;
+    private int maxHealth = 10;
+    private float moveSpeed = 4f;
+    private float fireRate = 0.5f;
+    private float shotSpeed = 3f;
+
+    public int Health { get => health; set => health = value; }
+    public int MaxHealth { get => maxHealth; set => maxHealth = value; }
+    public float MoveSpeed { get => moveSpeed; set => moveSpeed = value; }
+    public float FireRate { get => fireRate; set => fireRate = value; }
+
+    public float ShotSpeed { get => shotSpeed; set => shotSpeed = value; }
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Isaac/RoomsController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomsController : MonoBehaviour { 
+    
+    public GameObject initialRoom;
+    public List<Room> allRooms = new List<Room>();
+    Room currentRoom;
+    Vector3 startNewRoom;
+
+    public GameObject player;
+    static public RoomsController instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Instantiate(initialRoom, gameObject.transform);
+        Instantiate(player, gameObject.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void NewRoom(string exit)
+    {
+
+
+        Debug.Log("Se llama a new room");
+        currentRoom = Instantiate(allRooms[Random.Range(0, allRooms.Count - 1)]);
+        currentRoom.transform.SetParent(this.transform, false);
+        startNewRoom = currentRoom.StartRight.transform.position;
+
+        Vector3 correction = new Vector3(
+            startNewRoom.x - currentRoom.StartLeft.transform.position.x,
+            startNewRoom.y - currentRoom.StartLeft.transform.position.y,
+            0);
+        currentRoom.transform.position = correction;
+    }
+    
+}
